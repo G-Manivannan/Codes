6c4762e4bd71d592895bca2e0cc7508c7a43fc39
G-Manivannan <66206878+G-Manivannan@users.noreply.github.com>
2020-06-11 18:03:06 +0530

    This is just Mad lib game

---
+++ Mad Lib Games.py
@@ -0,0 +1,15 @@
+
+#this is three variable
+Colour = input("Enter the colour:")
+Plural_Noun = input("Enter the Plural_Noun:")
+Celebrity = input("Enter a Celebrity")
+
+
+print("Roses are "+ Colour)
+print(Plural_Noun +" is Blue")
+print("I Love "+ Celebrity)
+
+
+
+
+
